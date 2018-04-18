99c3c8c817bb90cbf32fda31fe1e799441cbf557
Branislav Holländer <branislav.hollander@gmail.com>
2018-04-18 07:49:29 +0200

    Allow saving the model

--- simple_lgbm.py
+++ simple_lgbm.py
@@ -9,7 +9,8 @@ train_sample_path = 'data/train_sample.csv'
 test_path = 'data/test.csv'
 
 start = datetime.now()
-VALIDATE = True
+VALIDATE = False
+TRAIN = True
 RANDOM_STATE = 50
 VALID_SIZE = 0.90
 MAX_ROUNDS = 1000
@@ -30,138 +31,142 @@ dtypes = {
         'click_id'      : 'uint32'
         }
 
-train_cols = ['ip','app','device','os', 'channel', 'click_time', 'is_attributed']
-train_df = pd.read_csv(train_path, skiprows=skiprows, nrows=nrows,dtype=dtypes, usecols=train_cols)
+if TRAIN:
+    train_cols = ['ip','app','device','os', 'channel', 'click_time', 'is_attributed']
+    train_df = pd.read_csv(train_path, skiprows=skiprows, nrows=nrows,dtype=dtypes, usecols=train_cols)
 
-len_train = len(train_df)
-gc.collect()
-
-most_freq_hours_in_test_data = [4, 5, 9, 10, 13, 14]
-least_freq_hours_in_test_data = [6, 11, 15]
-
-def prep_data(df):
-    
-    df['hour'] = pd.to_datetime(df.click_time).dt.hour.astype('uint8')
-    df['day'] = pd.to_datetime(df.click_time).dt.day.astype('uint8')
-    df.drop(['click_time'], axis=1, inplace=True)
-    gc.collect()
-    
-    df['in_test_hh'] = (   3 
-                         - 2*df['hour'].isin(  most_freq_hours_in_test_data ) 
-                         - 1*df['hour'].isin( least_freq_hours_in_test_data ) ).astype('uint8')
-    gp = df[['ip', 'day', 'in_test_hh', 'channel']].groupby(by=['ip', 'day', 'in_test_hh'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_day_test_hh'})
-    df = df.merge(gp, on=['ip','day','in_test_hh'], how='left')
-    df.drop(['in_test_hh'], axis=1, inplace=True)
-    df['nip_day_test_hh'] = df['nip_day_test_hh'].astype('uint32')
-    del gp
+    len_train = len(train_df)
     gc.collect()
 
-    gp = df[['ip', 'day', 'hour', 'channel']].groupby(by=['ip', 'day', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_day_hh'})
-    df = df.merge(gp, on=['ip','day','hour'], how='left')
-    df['nip_day_hh'] = df['nip_day_hh'].astype('uint16')
-    del gp
-    gc.collect()
-    
-    gp = df[['ip', 'os', 'hour', 'channel']].groupby(by=['ip', 'os', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_os'})
-    df = df.merge(gp, on=['ip','os','hour'], how='left')
-    df['nip_hh_os'] = df['nip_hh_os'].astype('uint16')
-    del gp
-    gc.collect()
+    most_freq_hours_in_test_data = [4, 5, 9, 10, 13, 14]
+    least_freq_hours_in_test_data = [6, 11, 15]
 
-    gp = df[['ip', 'app', 'hour', 'channel']].groupby(by=['ip', 'app',  'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_app'})
-    df = df.merge(gp, on=['ip','app','hour'], how='left')
-    df['nip_hh_app'] = df['nip_hh_app'].astype('uint16')
-    del gp
-    gc.collect()
+    def prep_data(df):
+        
+        df['hour'] = pd.to_datetime(df.click_time).dt.hour.astype('uint8')
+        df['day'] = pd.to_datetime(df.click_time).dt.day.astype('uint8')
+        df.drop(['click_time'], axis=1, inplace=True)
+        gc.collect()
+        
+        df['in_test_hh'] = (   3 
+                            - 2*df['hour'].isin(  most_freq_hours_in_test_data ) 
+                            - 1*df['hour'].isin( least_freq_hours_in_test_data ) ).astype('uint8')
+        gp = df[['ip', 'day', 'in_test_hh', 'channel']].groupby(by=['ip', 'day', 'in_test_hh'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_day_test_hh'})
+        df = df.merge(gp, on=['ip','day','in_test_hh'], how='left')
+        df.drop(['in_test_hh'], axis=1, inplace=True)
+        df['nip_day_test_hh'] = df['nip_day_test_hh'].astype('uint32')
+        del gp
+        gc.collect()
 
-    gp = df[['ip', 'device', 'hour', 'channel']].groupby(by=['ip', 'device', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_dev'})
-    df = df.merge(gp, on=['ip','device','hour'], how='left')
-    df['nip_hh_dev'] = df['nip_hh_dev'].astype('uint32')
-    del gp
-    gc.collect()
+        gp = df[['ip', 'day', 'hour', 'channel']].groupby(by=['ip', 'day', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_day_hh'})
+        df = df.merge(gp, on=['ip','day','hour'], how='left')
+        df['nip_day_hh'] = df['nip_day_hh'].astype('uint16')
+        del gp
+        gc.collect()
+        
+        gp = df[['ip', 'os', 'hour', 'channel']].groupby(by=['ip', 'os', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_os'})
+        df = df.merge(gp, on=['ip','os','hour'], how='left')
+        df['nip_hh_os'] = df['nip_hh_os'].astype('uint16')
+        del gp
+        gc.collect()
 
-    df.drop( ['ip','day'], axis=1, inplace=True )
-    gc.collect()
-    return df
+        gp = df[['ip', 'app', 'hour', 'channel']].groupby(by=['ip', 'app',  'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_app'})
+        df = df.merge(gp, on=['ip','app','hour'], how='left')
+        df['nip_hh_app'] = df['nip_hh_app'].astype('uint16')
+        del gp
+        gc.collect()
 
-train_df = prep_data(train_df)
-gc.collect()
+        gp = df[['ip', 'device', 'hour', 'channel']].groupby(by=['ip', 'device', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_dev'})
+        df = df.merge(gp, on=['ip','device','hour'], how='left')
+        df['nip_hh_dev'] = df['nip_hh_dev'].astype('uint32')
+        del gp
+        gc.collect()
 
-params = {
-          'boosting_type': 'gbdt',
-          'objective': 'binary',
-          'metric':'auc',
-          'learning_rate': 0.1,
-          'num_leaves': 9,  # we should let it be smaller than 2^(max_depth)
-          'max_depth': 5,  # -1 means no limit
-          'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
-          'max_bin': 100,  # Number of bucketed bin for feature values
-          'subsample': 0.9,  # Subsample ratio of the training instance.
-          'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
-          'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
-          'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
-          'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
-          'nthread': 8,
-          'verbose': 0,
-          'scale_pos_weight':99.7, # because training data is extremely unbalanced 
-         }
+        df.drop( ['ip','day'], axis=1, inplace=True )
+        gc.collect()
+        return df
 
-target = 'is_attributed'
-predictors = ['app','device','os', 'channel', 'hour', 'nip_day_test_hh', 'nip_day_hh', 'nip_hh_os', 'nip_hh_app', 'nip_hh_dev']
-categorical = ['app', 'device', 'os', 'channel', 'hour']
-
-
-if VALIDATE:
-    train_df, val_df = train_test_split(train_df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
-    dtrain = lgb.Dataset(train_df[predictors].values, 
-                         label=train_df[target].values,
-                         feature_name=predictors,
-                         categorical_feature=categorical)
-    del train_df
+    train_df = prep_data(train_df)
     gc.collect()
 
-    dvalid = lgb.Dataset(val_df[predictors].values,
-                         label=val_df[target].values,
-                         feature_name=predictors,
-                         categorical_feature=categorical)
-    del val_df
+    params = {
+            'boosting_type': 'gbdt',
+            'objective': 'binary',
+            'metric':'auc',
+            'learning_rate': 0.1,
+            'num_leaves': 9,  # we should let it be smaller than 2^(max_depth)
+            'max_depth': 5,  # -1 means no limit
+            'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+            'max_bin': 100,  # Number of bucketed bin for feature values
+            'subsample': 0.9,  # Subsample ratio of the training instance.
+            'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+            'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
+            'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+            'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+            'nthread': 8,
+            'verbose': 0,
+            'scale_pos_weight':99.7, # because training data is extremely unbalanced 
+            }
+
+    target = 'is_attributed'
+    predictors = ['app','device','os', 'channel', 'hour', 'nip_day_test_hh', 'nip_day_hh', 'nip_hh_os', 'nip_hh_app', 'nip_hh_dev']
+    categorical = ['app', 'device', 'os', 'channel', 'hour']
+
+    if VALIDATE:
+        train_df, val_df = train_test_split(train_df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
+        dtrain = lgb.Dataset(train_df[predictors].values, 
+                            label=train_df[target].values,
+                            feature_name=predictors,
+                            categorical_feature=categorical)
+        del train_df
+        gc.collect()
+
+        dvalid = lgb.Dataset(val_df[predictors].values,
+                            label=val_df[target].values,
+                            feature_name=predictors,
+                            categorical_feature=categorical)
+        del val_df
+        gc.collect()
+
+        evals_results = {}
+
+        model = lgb.train(params, 
+                        dtrain, 
+                        valid_sets=[dtrain, dvalid], 
+                        valid_names=['train','valid'], 
+                        evals_result=evals_results, 
+                        num_boost_round=MAX_ROUNDS,
+                        early_stopping_rounds=EARLY_STOP,
+                        verbose_eval=50, 
+                        feval=None)
+
+        del dvalid
+    else:
+        gc.collect()
+        dtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
+                            feature_name=predictors,
+                            categorical_feature=categorical
+                            )
+        del train_df
+        gc.collect()
+
+        evals_results = {}
+
+        model = lgb.train(params, 
+                        dtrain, 
+                        valid_sets=[dtrain], 
+                        valid_names=['train'], 
+                        evals_result=evals_results, 
+                        num_boost_round=OPT_ROUNDS,
+                        verbose_eval=50,
+                        feval=None)
+        
+    del dtrain
     gc.collect()
 
-    evals_results = {}
-
-    model = lgb.train(params, 
-                      dtrain, 
-                      valid_sets=[dtrain, dvalid], 
-                      valid_names=['train','valid'], 
-                      evals_result=evals_results, 
-                      num_boost_round=MAX_ROUNDS,
-                      early_stopping_rounds=EARLY_STOP,
-                      verbose_eval=50, 
-                      feval=None)
-
-    del dvalid
+    model.save_model('model.txt')
 else:
-    gc.collect()
-    dtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
-                          feature_name=predictors,
-                          categorical_feature=categorical
-                          )
-    del train_df
-    gc.collect()
-
-    evals_results = {}
-
-    model = lgb.train(params, 
-                      dtrain, 
-                      valid_sets=[dtrain], 
-                      valid_names=['train'], 
-                      evals_result=evals_results, 
-                      num_boost_round=OPT_ROUNDS,
-                      verbose_eval=50,
-                      feval=None)
-    
-del dtrain
-gc.collect()
+    model = lgb.Booster(model_file='model.txt')
 
 test_cols = ['ip','app','device','os', 'channel', 'click_time', 'click_id']
 test_df = pd.read_csv(test_path, dtype=dtypes, usecols=test_cols)
